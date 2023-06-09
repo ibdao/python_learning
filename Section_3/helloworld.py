bd11005d70615f2a2fa5176a28de4644574dfed1
iada-odoo <iada@odoo.com>
2023-06-08 21:15:41 -0700

    strings

---
+++ Section_3/helloworld.py
@@ -0,0 +1,10 @@
+# The standard first python program of every programming course ever.
+print("Hello World")
+print (1 + 2)
+print (7 * 6)
+print()
+print("The end", "or is it?", "keep watching to learn more about Python", 3)
+
+# Exercise 1
+print("My hovercraft is full of eels")
+print(6*7)
--- Section_3/test.py
+++
@@ -1,6 +0,0 @@
-# The standard first python program of every programming course ever.
-print("Hello World")
-print (1 + 2)
-print (7 * 6)
-print()
-print("The end", "or is it?", "keep watching to learn more about Python", 3)
