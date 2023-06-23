95c64a54071b9b1efac3d24ac3fcba334957dcbb
iada-odoo <iada@odoo.com>
2023-06-22 21:54:07 -0700

    movin on

---
+++ Section_3/escape_char.py
@@ -0,0 +1,7 @@
+split_string = "This string has been\nsplit over\nseveral\nlines."
+print(split_string)
+
+tab_string = "1\t2\t3\t4"
+print(tab_string)
+
+print('The pet shop owner said "No, no, \'e\'s uh,... he\'s resting".')
--- Section_3/strings.py
+++ Section_3/strings.py
@@ -3,3 +3,9 @@ print ('Python is fun')
 print ("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world") # string concatenation
+
+greeting = "Hello"
+name = input("Please enter your name ")
+
+print(greeting + ' ' + name)
+
