49988ea3b13a049ca4ece82ae6f4aa6b623784c7
iada-odoo <iada@odoo.com>
2023-06-30 14:53:11 -0700

    yer

--- Section_3/strings2.py
+++ Section_3/strings2.py
@@ -3,12 +3,25 @@ goal_string = "we win"
 
 print(parrot)
 
-for i in goal_string:
-    for j in range(0, len(parrot)):
-        if parrot[j] == i:
-            print(parrot[j])
-            break
-    
+# for i in goal_string:
+#     for j in range(0, len(parrot)):
+#         if parrot[j] == i:
+#             print(parrot[j])
+#             break
+# print()
 
-    
+# for i in goal_string:
+#     for j in range(0, len(parrot))[::-1]:
+#         if parrot[j] == i:
+#             print(parrot[j])
+#             break
+
+print(parrot[0:6]) # Norweg
+print(parrot[3:5]) # we
+print(parrot[0:9]) # Norwegian
+print(parrot[10:]) # Blue
+
+print(parrot[:6] + parrot[6:] == parrot[:])
+
+letters ="abcedefghijklmnopqrstuwxyz"
 
