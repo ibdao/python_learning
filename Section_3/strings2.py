53833a60e1c4cfe434966a8e9a3dd29eb7a7d75a
Ian <iada@odoo.com>
2023-06-30 13:43:23 -0700

    refactored

--- Section_3/strings2.py
+++ Section_3/strings2.py
@@ -4,9 +4,10 @@ goal_string = "we win"
 print(parrot)
 
 for i in goal_string:
-    for j in range(0, len(parrot) - 1):
+    for j in range(0, len(parrot)):
         if parrot[j] == i:
             print(parrot[j])
+            break
     
 
     
