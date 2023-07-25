ea6e854f584fde51882691bdbd73f9953c511878
iada-odoo <iada@odoo.com>
2023-07-25 15:19:16 -0700

    nested forloops

--- Section_4/conditions.py
+++ Section_4/conditions.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+
 age = int(input("How old are you?  "))
-if 16 <= age <= 65:
+if age in range(16, 66):
     print("Have a good day at work")
 else:
     print("Enough your free time")
-print("*" * 80)
--- Section_4/ranges.py
+++ Section_4/ranges.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python3
+for i in range(1, 21):
+    print(f"i is now {i}")
