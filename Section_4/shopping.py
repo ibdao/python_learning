cdc9cd84188f73d0f29fb8793224689a61dc744c
iada-odoo <iada@odoo.com>
2023-08-14 22:37:42 -0700

    new

---
+++ Section_4/shopping.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+
+for item in shopping_list:
+    if item == "spam":
+        continue
+    print(f"Buy {item}")
+
