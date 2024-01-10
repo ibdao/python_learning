bd01324ddf5083e5c941c0746329b60d09756000
iada-odoo <iada@odoo.com>
2024-01-09 16:24:59 -0800

    finish this

--- Section_5/number_list.py
+++ Section_5/number_list.py
@@ -1,10 +1,14 @@
+empty_list = []
 even = [2,4,6,8]
 odd = [1,3,5,9]
+numbers = even + odd
+print(numbers)
 
-even.extend(odd)
-another_even = even
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
 
-print(another_even)
-even.sort(reverse=True)
-print(sorted(even, reverse=True))
+# .sort method changes the list in place without creating a new list.
+# sorted() function sorts and creates a new list
 
+digits = sorted("432985617")
+print(digits) 
